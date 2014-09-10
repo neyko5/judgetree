da31723dee218df35d2de05232892633f8dc1f8e
Jernej Lipovec <neykomir@gmail.com>
2014-09-11 00:17:18 +0200

    small adjustments

--- app/controllers/tree_controller.rb
+++ app/controllers/tree_controller.rb
@@ -18,7 +18,7 @@ class TreeController < ApplicationController
     data_table.new_column('string', 'Tip')
     data_table.add_rows(Judge.where(country_tree: params[:id].upcase).asc(:name).collect{|j| [{
     	v: j.id.to_s,
-    	f:"<div class='judge #{j.level_to_word}'><p>#{j.name_output}</p><img class='image-flag' src='/assets/flags/#{j.country.downcase}.png' title='#{IsoCountryCodes.find(j.country).name}' /><div class='level level-#{j.level_to_short}'>#{j.level_to_short}</div><img class='profile' src='http://apps.magicjudges.org/dci/avatar?dci=#{j.dci}&size=120' /></div>"}, j.parent_judge.try(:id).try(:to_s), "#{j.name} (#{j.level})"]})
+    	f:"<div class='judge #{j.level_to_word}'><p>#{j.name_output}</p><img class='image-flag' src='/assets/flags/#{j.country.downcase}.png' title='#{IsoCountryCodes.find(j.country).name}' /><div class='level level-#{j.level_to_short}'>#{j.level_to_short}</div><img class='profile' src='http://apps.magicjudges.org/dci/avatar?dci=#{j.dci_number}&size=120' /></div>"}, j.parent_judge.try(:id).try(:to_s), "#{j.name} (#{j.level})"]})
 
     @chart = GoogleVisualr::Interactive::OrgChart.new(data_table, {allowHtml: true})
   end
